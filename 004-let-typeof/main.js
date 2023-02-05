3b20ea149391c10474d25c9114c036da20092eb5
Zicky78 <16zns01@gmail.com>
2023-02-05 15:51:30 -0800

    typeof and let

---
+++ 004-let-typeof/main.js
@@ -0,0 +1,11 @@
+let myName = 'Zachary Smith';
+
+let age = 24;
+
+let isProgramming = true;
+
+console.log(`${myName} is ${age} years old. Programming: ${isProgramming}`);
+
+console.log('Variable "myName": ' + typeof myName);
+console.log('Variable "age": ' + typeof age);
+console.log('Variable "isProgramming": ' + typeof isProgramming);
