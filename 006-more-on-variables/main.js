004ecb00e189b81f35279cede211b0837d4e5e7c
Zachary <16zns01@gmail.com>
2023-02-20 13:40:51 -0800

    more on variables

---
+++ 006-more-on-variables/main.js
@@ -0,0 +1,31 @@
+let myName = '24'
+let myAge = 24
+let myHappiness = true
+let newVar = 'SOMETHING'
+const myConst = 42
+
+console.log(`myName is typeof ${typeof myName}`)
+
+// Scope
+// if (true) {
+
+//     const myConst = 43
+//     console.log(myConst)
+    
+// } else {
+
+// }
+
+// Equality Operator
+console.log(myAge == myName) // Tries to convert
+
+console.log(myAge === myName) // Strictly equal
+
+// Undefined
+let myVar
+console.log(`myVar is tyepof ${ typeof myVar}`)
+
+let newestVar = null
+console.log(`myVar is also typeof ${typeof newestVar}`)
+
+
