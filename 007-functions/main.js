abbb431cbae5db44a442b726b0eaf427195a5057
Zachary <16zns01@gmail.com>
2023-02-20 15:19:34 -0800

    my functions

--- 007-functions/main.js
+++ 007-functions/main.js
@@ -18,12 +18,47 @@
 //-------------------------------------------------------
 
 //-------------------------------------------------------
-function doMath (a, b) {
-    console.log(`Result: ${a + b}`)
-    console.log(`Result: ${a - b}`)
-    console.log(`Result: ${a * b}`)
-    console.log(`Result: ${a / b}`)
+// function doMath (a, b) {
+//     console.log(`Result: ${a + b}`)
+//     console.log(`Result: ${a - b}`)
+//     console.log(`Result: ${a * b}`)
+//     console.log(`Result: ${a / b}`)
+// }
+
+// doMath(10, 4)
+//-------------------------------------------------------
+
+//-------------------------------------------------------
+// function doMath (a, b) {
+//         let result = a * b
+//         return result
+//     }
+    
+// let returnValue = doMath(10, 4) // console.log(doMath(10, 4)) 
+// console.log(`this result was returned from the function: ${returnValue}`)
+//-------------------------------------------------------
+
+//-------------------------------------------------------
+// function doMath (a, b) {
+//     let result = a * b
+//     return result
+// }
+
+// const result = doMath(10, 4) 
+// console.log(`this result was returned from the function: ${result}`)
+//-------------------------------------------------------
+
+// ----- MY VARIABLE CODE -----
+
+function calcBudget(paycheck, utilities, rent, phone, car, subscriptions){
+
+    let budget = paycheck - utilities - rent - phone - car - subscriptions
+    let savings = budget*0.45
+    balance = budget - savings
+    let budgetReport = `The remaining budget after bills is: $${budget} \n$${savings}(45%) should go into savings \nRemaining total is: $${balance}`
+
+    return budgetReport
+    
 }
 
-doMath(10, 4)
-//-------------------------------------------------------
+console.log(calcBudget(2500, 400, 370, 100, 415, 30))
