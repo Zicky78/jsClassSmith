0b8eace477c91c83c21f6acaed38c84be22e1ac5
Zachary <16zns01@gmail.com>
2023-02-21 15:07:40 -0800

    my arrow function - resubmit

--- 007-functions/main.js
+++ 007-functions/main.js
@@ -54,7 +54,7 @@
 
 //     let budget = paycheck - utilities - rent - phone - car - subscriptions
 //     let savings = budget*0.45
-//     balance = budget - savings
+//     let balance = budget - savings
 //     let budgetReport = `The remaining budget after bills is: $${budget} \n$${savings}(45%) should go into savings \nRemaining total is: $${balance}`
 
 //     return budgetReport
@@ -64,8 +64,21 @@
 // console.log(calcBudget(2500, 400, 370, 100, 415, 30))
 
 //-------------------------------------------------------
-doMoreMath = (a, b) => a * b
-console.log(doMoreMath(20, 30))
+// doMoreMath = (a, b) => a * b
+// console.log(doMoreMath(20, 30))
 //-------------------------------------------------------
 
+// ----- MY ARROW FUNCTION -----
 
+calcBudget = (paycheck, utilities, rent, phone, car, subscriptions) => {
+
+    let budget = paycheck - utilities - rent - phone - car - subscriptions
+    let savings = budget*0.45
+    let balance = budget - savings // I didn't notice I forgot let here for my other submission, but it didnt throw an error so I missed it
+    let budgetReport = `The remaining budget after bills is: $${budget} \n$${savings}(45%) should go into savings \nRemaining total is: $${balance}`
+
+    return budgetReport
+    
+}
+
+console.log(calcBudget(2500, 400, 370, 100, 415, 30))
