edabde047ec14970c9c8c5ada2af1e6fd9560d76
Zachary <16zns01@gmail.com>
2023-02-22 14:01:05 -0800

    learning about arrays

--- 008-more-functions/README.md
+++ 008-more-functions/README.md
@@ -1,1 +1,9 @@
+# Week 7
+
+# Are you using Dev Tools?
+
 ### Yes, I definitely use dev tools. Saves me a ton of time debugging when things inevitably go wrong
+
+# Does this array concept make sense to you?
+
+### Yep, arrays are great!
--- 008-more-functions/main.js
+++ 008-more-functions/main.js
@@ -15,3 +15,26 @@ updateDOM(mileage(300, 10, 5.40))
 const acceleration = (initialV, finalV, startTime, endTime) => { return (finalV-initialV)/(endTime-startTime)}
 
 updateDOM(`${acceleration(20, 39, 0, 3)} m/s\u{00B2}`)
+
+const myArr = []
+
+const trackMPGCost = (miles, gallons, price = 3.79) => {
+    const MPG = miles/gallons 
+    const tripCost = MPG * price
+    myArr.push(MPG, tripCost)
+}
+
+updateDOM(trackMPGCost(300, 10, 5.40))
+updateDOM(trackMPGCost(320, 20, 3.20))
+
+const accArr = []
+
+const trackAcceleration = (initialV, finalV, startTime = 0, endTime) => {
+    const acc = (finalV-initialV)/(endTime-startTime)
+    accArr.push(acc)
+    return acc
+    
+}
+
+updateDOM(`${trackAcceleration(20, 39, 0, 3)} m/s\u{00B2}`)
+updateDOM(`${trackAcceleration(5, 29, 0, 6)} m/s\u{00B2}`)
