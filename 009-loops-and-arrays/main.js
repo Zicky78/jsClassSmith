03b4fbf11283c9f297441d3309260f2a83afc26a
Zachary <16zns01@gmail.com>
2023-02-22 14:50:54 -0800

    writing the average MPG using for loop

--- 009-loops-and-arrays/main.js
+++ 009-loops-and-arrays/main.js
@@ -6,22 +6,34 @@ const updateDOM = (input) => {
     divEL.appendChild(h2)
 }
 
-const myArr = []
+const myMPG = []
+const myTripCost = []
 
 const trackMPGCost = (miles, gallons, price = 3.79) => {
-    const MPG = miles/gallons 
+    const MPG = miles/gallons
     const tripCost = gallons * price
-    myArr.push(MPG, tripCost)
-    return `MPG: ${MPG} \n Trip Cost: $${tripCost}`
+    myMPG.push(MPG)
+    myTripCost.push(tripCost)
+    return `MPG: ${MPG.toFixed(2)} \n Trip Cost: $${tripCost.toFixed(2)}`
+}
+
+const calculateAvgMPG = () => {
+    let totalMPG = 0
+    for(let i = 0; i < myMPG.length; i++){
+        totalMPG += myMPG[i]
+    }
+
+    let avgMPG = totalMPG/myMPG.length
+    updateDOM(`Average MPG is ${avgMPG.toFixed(2)}`)
 }
 
 updateDOM(trackMPGCost(300, 10, 5.40))
 updateDOM(trackMPGCost(320, 20, 3.20))
-updateDOM(myArr)
+updateDOM(trackMPGCost(100, 7, 4.40))
+updateDOM(trackMPGCost(600, 24, 5.70))
+updateDOM(trackMPGCost(50, 2, 3.40))
+updateDOM(trackMPGCost(320, 20, 3.20))
+updateDOM(trackMPGCost(320, 12, 5.00))
 
-const calculateAvg = () => {
-    const avgMPG = myArr[0] + myArr[2] / 2
-    updateDOM(`Average Miles Per Gallon is: ${avgMPG}`)
-}
+calculateAvgMPG()
 
-calculateAvg()
