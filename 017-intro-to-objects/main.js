3c65a360048f013e6f74d2afe44fcfa4e9b065a0
Zachary <16zns01@gmail.com>
2023-03-06 14:59:30 -0800

    += objects

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -8,6 +8,7 @@ const myNumber = 9
 const myObj = {
     name: 'Zach',
     age: 24,
+    likes: 0,
     hobbies: ['Coding', 'Piano', 'Video Games'],
     mobile: {
         make: 'Samsung',
@@ -16,6 +17,9 @@ const myObj = {
     },
     myGreeting: function(person) {
         return `${this.name}, who plays too many ${this.hobbies[2]} says: "Whats up, ${person}?"`
+    },
+    increaseLikes: function() {
+        this.likes += 1
     }
 }
 
@@ -23,7 +27,7 @@ const greeting = myObj.myGreeting('Bobby')
 console.log(greeting)
 
 const student = {
-    name: 'Bob',
+    name: 'Bill',
     studentId: '123-4567',
     testScores: [],
     academicProbation: false,
@@ -43,3 +47,41 @@ student.addTestScore(79)
 student.addTestScore(84)
 student.addTestScore(91)
 console.log(student.calculateGrade().toFixed(2))
+
+console.log(myObj.name[1])
+
+// Testing some methods
+
+// String.repeat(n) and String.toUpperCase()
+function cheerFor(student) {
+    let cheer = student.name + '! '
+    console.log(cheer.toUpperCase().repeat(5))
+}
+
+cheerFor(student)
+
+// Array.map() 
+function curveGrading(student) {
+    console.log(student.testScores.map(grade => grade*1.15))
+}
+
+curveGrading(student)
+
+// Array.reduce()
+// Also ternaries are fun, even if they are sometimes less readable
+// Admittedly I haven't used map/reduce a whole lot even though they are super useful, so this was good practice
+function highestTestScore(student) {
+    console.log(student.testScores.reduce((prev, cur) => cur > prev ? cur : prev )) 
+}
+
+highestTestScore(student)
+
+for(key in myObj) {
+    console.log(`key ${key} and value ${myObj[key]}`)
+}
+
+const zachObj = myObj
+zachObj.increaseLikes()
+console.log(zachObj.likes)
+console.log(myObj.likes)
+zachObj = 25
