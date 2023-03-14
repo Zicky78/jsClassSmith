dba4f494bd03846c619efadcbce21cd57a886a5b
Zachary <16zns01@gmail.com>
2023-03-14 16:22:13 -0700

    listeners for buttons

---
+++ 022-handle-edit/README.md
@@ -0,0 +1,29 @@
+# Week 10
+
+## function vs =>
+
+My philosophy is that I use function syntax if the code inside will be reused in other places in my program, and arrow functions 
+for callbacks or quick one-line functions, or things that only get used once. function syntax is cleaner, because you don't use const to get 
+confused with other variables at a glance. I love arrow functions though, they're really nice utility
+
+## Where in the code do we start to render the table?
+
+After we get our data and do our calculations, near where we are calling the other outputs
+
+## How do we get the next rows with the data we need in them?
+
+We could loop over MY_DATA with a forEach to create the table rows, and then run a for in on each of those objects to create our table data
+
+## How do we fix the data lining up with the table?
+
+Thankfully my code was already set up in the correct order, but we can just reorder the key/values in the object
+
+## Separating the render and populate functions
+
+I made a new function populateTableData, and grabbed the previously made table with a querySelector
+I spent a good while thinking about how I could not re-render the whole table each time, but I couldn't
+figure it out without refactoring quite a bit. Since we loop through all the data to create our nodes, they get appended multiple times. Clearing just the table's innerHTML also gets rid of the headings, so I also thought about getting a list of all child elements of the table and deleting all but the first, but I figured I'd just move on and see how you did it. 
+
+## Final
+
+I understood all of the code and it makes sense to me
---
+++ 022-handle-edit/main.js
@@ -0,0 +1,100 @@
+function updateDOM(input, id){
+	const divEL = document.getElementById(id);
+	const h3 = document.createElement('h3');
+	h3.textContent = input;
+	divEL.appendChild(h3);
+};
+
+const MY_DATA = []
+const FORM_EL = document.getElementById('form-input')
+const TBL_OUTPUT = document.getElementById('table-out')
+
+function calcAvg() {
+	document.getElementById('avg-output').innerHTML = ''
+	let avgMPG = MY_DATA.reduce((sum, MY_DATA) => sum + MY_DATA.MPG, 0) / MY_DATA.length
+	let avgTripCost = MY_DATA.reduce((sum, MY_DATA) => sum + MY_DATA.tripCost, 0) / MY_DATA.length
+	updateDOM(`Average MPG: ${avgMPG.toFixed(2)} | Average Trip Cost: $${avgTripCost.toFixed(2)}`, 'avg-output')
+}
+
+function validateForm(miles, gallons, price) {
+	document.getElementById('err').innerHTML = ''
+	const errMsg = []
+	if(miles <= 0 || gallons <= 0 || price <= 0) {
+		errMsg.push('Please enter a value greater than 0. ')
+	}
+	if(price > 1000) {
+		errMsg.push(`Paying more than $1000/gal for gas is the real error here. Just bike at that point.`)
+	}
+	if(errMsg.length > 0) {
+		updateDOM(errMsg, 'err')
+	} else {
+		return true 
+	}
+}
+
+function renderEditDelBtn(index) {
+	const td = document.createElement('td')
+	const editBtn = document.createElement('button')
+	editBtn.textContent = 'Edit'
+	const delBtn = document.createElement('button')
+	delBtn.textContent = 'Delete'
+	// Add event listener
+	// For edit: reference the object in the array for the current row in the table and populate the form input fields
+	editBtn.addEventListener('click', (e) => {
+		console.log(e, index)
+	})
+	td.appendChild(editBtn)
+	td.appendChild(delBtn)
+	return td
+}
+
+function renderTable() {
+	TBL_OUTPUT.innerHTML = ''
+	const tbl = document.createElement('table')
+	const headings = ['Miles Driven', 'Gallons Used', 'Price Paid', 'Trip MPG', 'Trip Cost', 'Edit/Delete']
+	const tr = document.createElement('tr')
+	headings.forEach((heading) => {
+		let th = document.createElement('th')
+		th.textContent = heading
+		tr.appendChild(th)
+	})
+	tbl.appendChild(tr)
+	TBL_OUTPUT.appendChild(tbl)
+}
+
+function populateTableData() {
+	let tbl = document.querySelector('table')
+	MY_DATA.forEach((entry, index) => {
+		let tr = document.createElement('tr')
+		for(key in entry) {
+			let td = document.createElement('td')
+			td.textContent = entry[key]
+			tr.appendChild(td)
+		}
+		const btnTD = renderEditDelBtn(index)
+		tr.appendChild(btnTD)
+		tbl.appendChild(tr)
+	})
+}
+
+FORM_EL.addEventListener('submit', (e) => {
+	e.preventDefault()
+	let miles = e.target.miles.value
+	let gallons = e.target.gallons.value
+	let price = e.target.price.value
+	const isValid = validateForm(miles, gallons, price)
+	if(isValid) {
+		MY_DATA.push(
+			{
+				miles: miles,
+				gallons: gallons,
+				price: price,
+				MPG: miles/gallons,
+				tripCost: gallons*price,
+			})
+			updateDOM(`#${MY_DATA.length} - MPG: ${MY_DATA[MY_DATA.length - 1].MPG.toFixed(2)} | Trip Cost: $${MY_DATA[MY_DATA.length - 1].tripCost.toFixed(2)}`, 'output')
+			renderTable()
+			populateTableData()
+			calcAvg()
+	}
+})
