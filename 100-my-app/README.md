6eedf212ad3d740a997a2974bf7b5b0730008811
Zachary <16zns01@gmail.com>
2023-04-25 13:25:57 -0700

    finish DOM render

--- 100-my-app/README.md
+++ 100-my-app/README.md
@@ -1,24 +1,8 @@
-## Week 13
+## Week 14
 
-Okay so I made my design in figma yesterday (monday), which I probably spent way more time on than necessary since I am not a designer, but still wanted things to look pretty haha. I included screenshots of it, though I was having trouble importing images to it for some reason so the edit / delete icon buttons are missing.
+This is as close as I can get it to my mock up, and I'm pretty happy with the results, despite it not being the most mobile friendly. It should only need minimal changes to the css when I add in the edit/delete buttons. 
 
-Next I asked ChatGPT to kindly make me a modal cause I didn't like the w3school's one. It forgot the open-modal button and function though so I had to remind it to add that (I don't have GPT4 yet), but after that I was able to cut/paste my form over into it no problem.
 
-I also spent some time asking it a lot of CSS-best-practices questions since I'm even rustier at CSS than designing. I was hoping there was a way to import / export the custom variables like you can for modules in JS, but it seems that for smaller projects with only a few variables its better to just re-define them to save on requests during page load time. 
-
-I got github copilot over the weekend and it was great for auto-completing CSS and keeping things semi-consistent. Once the bulk of the CSS is done I need to go back in and make things more standardized, but I wouldn't have gotten nearly as far into completing the CSS doing it on my own. So far I haven't really found much use for it beyond a more intelligent auto-complete though.
-
-I think this is a good stopping point for this assignment, the rest of it is fine tuning the CSS to get as close to my mockup as possible, and adjusting my rendering function to match, which will be done for week 14.
-
-Week 15 - Edit and Delete
-
-Week 16 - Local Storage
-
-Week 17 - AI's gain sentience and self-awareness
-
-Week 18 - Survive the AI apocalypse and still somehow try to pass finals
-
-I also need to add functionality to close the modal upon submit.
 
 
 
--- 100-my-app/render.js
+++ 100-my-app/render.js
@@ -2,42 +2,47 @@ import { calLog, entry } from "./log.js";
 
 const exerciseCheckBox = document.getElementById("exercise");
 const calBurnedInput = document.getElementById("cal-burned-input");
+const calBurned = document.getElementById("calBurned");
 const entryOutput = document.getElementById("entry-output");
 const ERR = document.getElementById("err");
+const exerciseOutput = document.getElementById("exercise-output");
+const calTotalOutput = document.getElementById("cal-total-output");
 
 // ChatGPT-generated modal code
-const modalContainer = document.getElementById('modal-container');
-const closeBtn = document.getElementById('close-modal-btn');
-const backdrop = document.getElementById('backdrop');
-const openBtn = document.getElementById('open-modal-btn');
+const modalContainer = document.getElementById("modal-container");
+const closeBtn = document.getElementById("close-modal-btn");
+const backdrop = document.getElementById("backdrop");
+const openBtn = document.getElementById("open-modal-btn");
 
 // Open the modal
 function openModal() {
-  modalContainer.style.display = 'block';
-  backdrop.style.display = 'block';
+	modalContainer.style.display = "block";
+	backdrop.style.display = "block";
 }
 
 // Close the modal
 function closeModal() {
-  modalContainer.style.display = 'none';
-  backdrop.style.display = 'none';
+	modalContainer.style.display = "none";
+	backdrop.style.display = "none";
 }
 
 // Open the modal when clicking the open button
-openBtn.addEventListener('click', openModal);
+openBtn.addEventListener("click", openModal);
 
 // Close the modal when clicking the close button
-closeBtn.addEventListener('click', closeModal);
+closeBtn.addEventListener("click", closeModal);
 
 // Close the modal when clicking outside the modal content
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener("click", closeModal);
 
 // Control if calories burned input is shown based on status of exercise check box
 exerciseCheckBox.addEventListener("change", (e) => {
 	if (exerciseCheckBox.checked) {
 		calBurnedInput.classList.remove("hidden");
+		calBurned.classList.remove("hidden");
 	} else {
 		calBurnedInput.classList.add("hidden");
+		calBurned.classList.add("hidden");
 	}
 });
 
@@ -54,32 +59,24 @@ function displayLogBook() {
 			itemOutput.classList.add("item-output");
 
 			let foodOutput = document.createElement("p");
+			foodOutput.classList.add("col");
 			foodOutput.textContent = item.food;
-			let amountOutput = document.createElement("p");
-			amountOutput.textContent = `Servings: ${item.amount} x ${item.calories}`;
 			let calorieOutput = document.createElement("p");
-			calorieOutput.textContent = `Calories: ${item.amount * item.calories}`;
+			calorieOutput.classList.add("col");
+			calorieOutput.textContent = `${item.calories}`;
+			let amountOutput = document.createElement("p");
+			amountOutput.classList.add("col");
+			amountOutput.textContent = `${item.amount}`;
 
 			itemOutput.appendChild(foodOutput);
-			itemOutput.appendChild(amountOutput);
 			itemOutput.appendChild(calorieOutput);
+			itemOutput.appendChild(amountOutput);
 
 			entryOutput.appendChild(itemOutput);
 		});
 
-		let totalOutputs = document.createElement("div");
-		totalOutputs.classList.add("total-outputs");
-
-		let exerciseOutput = document.createElement("h3");
-		exerciseOutput.textContent = `Exercise: ${entry.calBurned} calories burned!`;
-		entryOutput.appendChild(exerciseOutput);
-
-		let calTotalOutput = document.createElement("h3");
-		calTotalOutput.textContent = `Total Calories Today: ${entry.calTotal}`;
-
-		totalOutputs.appendChild(exerciseOutput);
-		totalOutputs.appendChild(calTotalOutput);
-		entryOutput.appendChild(totalOutputs);
+		exerciseOutput.textContent = `${entry.calBurned}`;
+		calTotalOutput.textContent = `${entry.calTotal}`;
 	});
 }
 
@@ -89,7 +86,5 @@ function displayErrors(errors) {
 	msg.textContent = errors;
 	ERR.appendChild(msg);
 }
-
-
 
 export { displayLogBook, displayErrors, ERR, calBurnedInput };
