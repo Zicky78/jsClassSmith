bf25db040fb5fdcc5942e16cd772bed975043e58
Zachary <16zns01@gmail.com>
2023-04-25 16:43:58 -0700

    real world example

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -39,35 +39,57 @@
 // // promise chain
 // getWeather().then(getWeatherIcon).then(onSuccess).catch(onError);
 
-function fun1() {
-	return new Promise(function (resolve, reject) {
-		setTimeout(() => {
-			reject("404");
-		}, 100);
-	});
-}
+// function fun1() {
+// 	return new Promise(function (resolve, reject) {
+// 		setTimeout(() => {
+// 			reject("404");
+// 		}, 100);
+// 	});
+// }
 
-function fun2() {
-    console.log("fun2");
-	return new Promise(function (resolve, reject) {
-		setTimeout(() => {
-			resolve("🧔");
-		}, 100);
-	});
-}
+// function fun2() {
+//     console.log("fun2");
+// 	return new Promise(function (resolve, reject) {
+// 		setTimeout(() => {
+// 			resolve("🧔");
+// 		}, 100);
+// 	});
+// }
 
-function onSuccess(data) {
-	console.log(data);
-}
-function onError(errorCode) {
-	console.log(`Error: ${errorCode}`);
-}
+// function onSuccess(data) {
+// 	console.log(data);
+// }
+// function onError(errorCode) {
+// 	console.log(`Error: ${errorCode}`);
+// }
 
-function onFinally() {
-    console.log("Finally we be done yo");
-}
+// function onFinally() {
+//     console.log("Finally we be done yo");
+// }
 
-fun1().then(fun2).then(onSuccess).catch(onError).finally(onFinally);
+// fun1().then(fun2).then(onSuccess).catch(onError).finally(onFinally);
 
 //.catch() is a global error handler for the chain
 //passing in the error handling function to the .then() method is a local error handler for that promise
+
+function fetchData() {
+    return new Promise(function (resolve, reject) {
+        // This should be the right code for Fresno
+        // Edit: Okay I know this is the right code because before typing the word Fresno,
+        // github copilot was able to recognize the link and fill in the word Fresno for me, just by
+        // seing the forecast office code and coordinates I guess haha. Wild.
+        fetch('https://api.weather.gov/gridpoints/HNX/36,119/forecast') 
+        .then(response => response.json())
+        .then(data => resolve(data.properties.periods[1].shortForecast))
+    });
+}
+
+function displayData(weather) {
+    console.log(weather);
+}
+
+function onError(error) {
+    console.log(`ERR: ${error}`);
+}
+
+fetchData().then(displayData).catch(onError);
