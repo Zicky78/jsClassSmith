b04ed6392492f228b5339a98875dae600c6965c7
Zachary <16zns01@gmail.com>
2023-04-26 17:22:40 -0700

    API Keys

--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -3,13 +3,18 @@ import { newsAPI, spotifyAPI } from "./api-key.js";
 let newsAPIKey = newsAPI.KEY;
 let spotifyAPIKey = spotifyAPI.KEY;
 
-const url = "http://worldtimeapi.org/api/timezone/America/New_York";
+// const url = "http://worldtimeapi.org/api/timezone/America/New_York";
+const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + newsAPIKey;
 
 async function getData() {
 	const response = await fetch(url);
 	console.log(response);
 	const data = await response.json();
-	console.log(data);
+    data.articles.forEach(article => {
+        console.log(article.title);
+    });
 }
 
 getData();
+
+
