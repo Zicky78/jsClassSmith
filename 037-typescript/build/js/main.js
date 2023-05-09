b5bfd3cb27a3a50ac76f190511fec4f6989d76b3
Zachary <16zns01@gmail.com>
2023-05-08 17:12:20 -0700

    finish basic types

--- 037-typescript/build/js/main.js
+++ 037-typescript/build/js/main.js
@@ -3,9 +3,16 @@ let myName;
 let meaningOfLife;
 let isLoading;
 let album;
+// let album: string | number;
 myName = "Zach";
 myName = "Zachary";
 meaningOfLife = 42;
 isLoading = true;
 album = "Van Halen";
-album = true;
+album = 5150;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- 037-typescript/src/main.ts
+++ 037-typescript/src/main.ts
@@ -2,10 +2,20 @@ let myName: string;
 let meaningOfLife: number;
 let isLoading: boolean;
 let album: any;
+// let album: string | number;
 
 myName = "Zach";
 myName = "Zachary";
 meaningOfLife = 42;
 isLoading = true;
 album = "Van Halen";
-album = true;
+album = 5150;
+
+const sum = (a: number, b: string) => {
+	return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g; 
